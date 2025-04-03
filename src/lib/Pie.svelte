1b4ba0ac4a9fd1abb1459fc58bdac0b617f25703
Gerardo Mikael do Carmo Pereira <B46994@fgv.edu.br>
2025-04-03 12:02:10 -0300

    Modificações no pie chart

---
+++ src/lib/Pie.svelte
@@ -0,0 +1,31 @@
+<script>
+import * as d3 from 'd3';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2];
+let colors = ['gold', 'purple'];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map(d => arcGenerator(d));
+
+</script>
+
+<svg viewBox="-50 -50 100 100">
+    {#each arcs as arc, index}
+    <path d={arc} fill={colors[index]} />
+    {/each}
+</svg>
+
+<style>
+
+svg {
+    max-width: 20em;
+    margin-block: 2em;
+
+    /* Do not clip shapes outside the viewBox */
+    overflow: visible;
+}
+
+</style>
